f980c0caff7e479af2b2010091d19b0a771bcd0d
oriarb13 <oriarb13690@gmail.com>
2025-06-02 17:57:02 +0300

    z

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings): # Class Inheritance:
     # jwt
     SECRET_KEY: str = "your-secret-key-here" 
     ALGORITHM: str = "HS256" 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1000 
     
     # CORS
     ALLOWED_HOSTS: List[str] = ["*"] 
